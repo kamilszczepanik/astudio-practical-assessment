385fe259fc21ca46628916601e75b00b0352d122
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-02-23 23:02:29 +0400

    bug: scroll and max height in dropdown

--- client/src/components/ui/dropdown-menu.tsx
+++ client/src/components/ui/dropdown-menu.tsx
@@ -77,7 +77,7 @@ const DropdownMenuContent = React.forwardRef<
 		<DropdownMenuPrimitive.Content
 			ref={ref}
 			sideOffset={sideOffset}
-			className={`bg-popover text-popover-foreground z-50 min-w-[8rem] overflow-hidden rounded-md border bg-white p-1 shadow-lg ${
+			className={`bg-popover text-popover-foreground z-50 max-h-96 min-w-[8rem] overflow-y-auto rounded-md border bg-white p-1 shadow-lg ${
 				className || ''
 			}`}
 			{...props}
